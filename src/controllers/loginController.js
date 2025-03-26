6898cb9a706a82fdd68c992f1e5acd4181998fca
wojteq89 <wojteq898@gmail.com>
2025-03-26 19:08:07 +0100

    Delete is_verified field need to be true while login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -16,9 +16,9 @@ exports.loginUser = async (req, res) => {
       return res.status(400).json({ message: 'Invalid email or password' });
     }
 
-    if (!user.is_verified) {
-      return res.status(400).json({ message: 'Please verify your email' });
-    }
+    // if (!user.is_verified) {
+    //   return res.status(400).json({ message: 'Please verify your email' });
+    // }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
