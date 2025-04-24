86b6a49c4a6ced5be748c59cd130d05cf4e92eca
BartekArseniuk <bartekarseniuk1701@gmail.com>
2025-04-24 09:02:26 +0200

    Add CORS middleware to handle cross-origin requests

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
 
