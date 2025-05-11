90c68e34dfc45a3f475336aa0a8262f9054f69e1
BartekArseniuk <bartekarseniuk1701@gmail.com>
2025-05-12 00:17:11 +0200

    Add user ID to JWT payload during login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -27,6 +27,7 @@ exports.loginUser = async (req, res) => {
 
     const token = jwt.sign(
       { 
+        id: user.id,
         email: user.email,
         first_name: user.first_name, 
         last_name: user.last_name,
